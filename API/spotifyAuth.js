d54c1fae0b816da5cbd5c80ec1eb11278b6b37f9
DmitryMakarich <63462326+DmitryMakarich@users.noreply.github.com>
2022-06-09 15:33:08 +0300

    Edit redirect url to deployed app

--- API/spotifyAuth.js
+++ API/spotifyAuth.js
@@ -1,11 +1,6 @@
 const client_id = "f0dab5f6825a4357bf322993dd2397dc";
-// const client_secret = "8ca86dd3c8b24a26a7df203a3506b089";
-// const redirect_uri = "http%3A%2F%2F127.0.0.1%3A5501%2Fhtml%2Fhome.html";
-// const unencoded_redirect_uri = "http://127.0.0.1:5501/html/home.html";
-// const redirect_uri = "http%3A%2F%2F127.0.0.1%3A5501%2F%23home.html";
-// const unencoded_redirect_uri = "http://127.0.0.1:5501/#home.html";
-const redirect_uri = "http%3A%2F%2F127.0.0.1%3A5501";
-const unencoded_redirect_uri = "http://127.0.0.1:5501";
+const redirect_uri = "https%3A%2F%2Fdmitrymakarich.github.io%2FSpotify%2F";
+const unencoded_redirect_uri = "https://dmitrymakarich.github.io/Spotify/";
 const scopes = "playlist-read-private";
 
 export const login = async () => {
